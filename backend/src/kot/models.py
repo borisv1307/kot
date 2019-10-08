5f09a1369faf2ffb70fe247aa17b57243915f664
rakhfa <ra693@drexel.edu>
2019-10-07 21:39:08 -0400

    removed user_id field from model. made username as primary key.

--- backend/src/kot/models.py
+++ backend/src/kot/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=30)
+    username = models.CharField(primary_key=True, max_length=30, default="sample")
     password = models.CharField(max_length=30)
     date_created = models.DateTimeField()
     date_modified = models.DateTimeField()
-    
+
+
 class Game(models.Model):
     GAME_RESULT = (
         ('Y', 'Yes'),
@@ -20,5 +21,6 @@ class Game(models.Model):
     date_created = models.DateTimeField()
     date_modified = models.DateTimeField()
     is_winner = models.CharField(max_length=1, choices=GAME_RESULT)
+
     class Meta:
         unique_together = (('game_id', 'user'),)
--- backend/src/kot/tests.py
+++ backend/src/kot/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from datetime import date
+from .models import User
+
 
 # Create your tests here.
+class UserModelTest(TestCase):
+
+    def setUp(self):
+        self.username = "testuser"
+        self.password = "testPassword"
+        self.date_created = date.today()
+        self.date_modified = date.today()
+
+        self.test_user = User.objects.create(
+            username=self.username,
+            password=self.password,
+            date_created=self.date_created,
+            date_modified=self.date_modified
+        )
+
+    def test_user_model(self):
+        user = self.test_user
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.username, self.username)
+        self.assertEqual(user.date_modified, self.date_modified)
+        self.assertEqual(user.date_created, self.date_created)
+        self.assertEqual(user.password, self.password)
