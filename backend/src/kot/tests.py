3509a06a679f17000cafef669a9a9a5ed6b9430e
rakhfa <ra693@drexel.edu>
2019-10-08 20:08:08 -0400

    removed user_id field from model. made username as primary key.

--- backend/src/kot/tests.py
+++ backend/src/kot/tests.py
@@ -1,22 +1,37 @@
 from django.test import TestCase
-from datetime import date
-from .models import User
+from datetime import datetime
+from django.utils import timezone
+from .models import User, Game
 
 
 # Create your tests here.
-class UserModelTest(TestCase):
+class ModelTest(TestCase):
+
+    id_counter = 0
 
     def setUp(self):
-        self.username = "testuser"
+        self.username = "kot_user"
         self.password = "testPassword"
-        self.date_created = date.today()
-        self.date_modified = date.today()
+        self.date_created = datetime.now(tz=timezone.utc)
+        self.date_modified = datetime.now(tz=timezone.utc)
 
         self.test_user = User.objects.create(
             username=self.username,
             password=self.password,
             date_created=self.date_created,
             date_modified=self.date_modified
+        )
+
+        self.game_id = 1
+        self.user = self.test_user
+        self.isWinner = 'N'
+
+        self.game = Game.objects.create(
+            game_id=self.game_id,
+            user=self.test_user,
+            date_created=self.date_created,
+            date_modified=self.date_modified,
+            is_winner=self.isWinner
         )
 
     def test_user_model(self):
@@ -26,3 +41,13 @@ class UserModelTest(TestCase):
         self.assertEqual(user.date_modified, self.date_modified)
         self.assertEqual(user.date_created, self.date_created)
         self.assertEqual(user.password, self.password)
+
+    def test_game_model(self):
+        game = self.game
+        self.assertIsInstance(game, Game)
+        self.assertEqual(game.game_id, self.game_id)
+        self.assertEqual(game.user, self.user)
+        self.assertEqual(game.date_created, self.date_created)
+        self.assertEqual(game.date_modified, self.date_modified)
+
+
