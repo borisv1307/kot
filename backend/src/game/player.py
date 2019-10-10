2bb1fc62c98027d26d332c9d67a5b8f2be5a9219
borisv13 <borisv13@gmail.com>
2019-10-10 12:56:02 -0400

    Removing comments

--- backend/src/game/player.py
+++ backend/src/game/player.py
@@ -1,10 +1,3 @@
-"""
-This is a class that keeps the state of a given player for the game King of
-Tokyo.
-Locations that the player can be:
-    - Outside: Denotes that the player is not on the game board.
-    - Tokyo: The player is in Tokyo
-"""
 from backend.src.game import constants
 from backend.src.game.locations import Locations
 
