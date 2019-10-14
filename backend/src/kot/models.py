2a7dc79719a74898737ee2f9ea0d9296a6ce7512
Oyesanmi <taiwo.oyesanmi@accenture.com>
2019-10-13 21:00:12 -0400

    updated database to support entire game

--- backend/src/kot/models.py
+++ backend/src/kot/models.py
@@ -2,8 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+# Monsters tend to change so they will be lookup 
+class Monster(models.Model):
+    monster_name = models.CharField(max_length=30)
+    date_created = models.DateTimeField()
+    date_modified = models.DateTimeField()
+    
+# load all 66 Cards     
+class Card(models.Model):
+    card_details = models.CharField(max_length=30)
+    date_created = models.DateTimeField()
+    date_modified = models.DateTimeField()
+
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
+    #ids are better for realtionship and reference, they are auto created in all classes
+    #user_id = models.AutoField(primary_key=True)
+    user_monster = models.ForeignKey(Monster, on_delete=models.CASCADE)
+    #the user can be an email or guest thus username 
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     date_created = models.DateTimeField()
@@ -15,10 +30,59 @@ class Game(models.Model):
         ('N', 'No'),
         ('I', 'In-Progress'),
     )
-    game_id = models.IntegerField(primary_key=True)
+    MONSTER_POSITION = (
+        ('T', 'In-Tokyo'),
+        ('O', 'Outside-Tokyo'),
+    )
+    #game_id = models.IntegerField(primary_key=True)
+    # should cascade be applied below.
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    position = models.CharField(max_length=1, choices=MONSTER_POSITION)
     date_created = models.DateTimeField()
     date_modified = models.DateTimeField()
     is_winner = models.CharField(max_length=1, choices=GAME_RESULT)
-    class Meta:
-        unique_together = (('game_id', 'user'),)
+    #class Meta:
+    #    unique_together = (('game_id', 'user'),)
+
+class Dice(models.Model):
+    DICE_VALUE = (
+        ('1', 'One'),
+        ('2', 'Two'),
+        ('3', 'Two'),
+        ('4', 'Energy'),
+        ('5', 'Attack'),
+        ('6', 'Heal'),
+    )
+    DICE_SELECT = (
+        ('Y', 'Selected'),
+        ('N', 'Not-Selected'),
+    )
+    # should cascade be applied below.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dice1 = models.CharField(max_length=1, choices=DICE_VALUE)
+    dice1_selected = models.CharField(max_length=1, choices=DICE_SELECT)
+    Dice2 = models.CharField(max_length=1, choices=DICE_VALUE)
+    dice2_selected = models.CharField(max_length=1, choices=DICE_SELECT)
+    Dice3 = models.CharField(max_length=1, choices=DICE_VALUE)
+    dice3_selected = models.CharField(max_length=1, choices=DICE_SELECT)
+    Dice4 = models.CharField(max_length=1, choices=DICE_VALUE)
+    dice4_selected = models.CharField(max_length=1, choices=DICE_SELECT)
+    Dice5 = models.CharField(max_length=1, choices=DICE_VALUE)
+    dice5_selected = models.CharField(max_length=1, choices=DICE_SELECT)
+    Dice6 = models.CharField(max_length=1, choices=DICE_VALUE)
+    dice6_selected = models.CharField(max_length=1, choices=DICE_SELECT)
+    date_created = models.DateTimeField()
+    date_modified = models.DateTimeField()
+    
+    
+class Play(models.Model):
+    # should cascade be applied below.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # should cascade be applied below.
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    victory_points = models.IntegerField()
+    energy_cube = models.IntegerField()
+    life_points = models.IntegerField()
+    date_created = models.DateTimeField()
+    date_modified = models.DateTimeField() 
+ 
--- backend/src/kot/tests.py
+++ backend/src/kot/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+import datetime as datetime_orig
+from .models import Monster
+from .models import Card
+from .models import User
+from .models import Game
+from .models import Dice
+from kot.models import Play
 
-# Create your tests here.
+class monster_model_test(TestCase):
+
+    @classmethod
+    def set_up_monster_test_data(cls):
+        Monster.objects.create(monster_name="Godzilla")
+        Monster.objects.create(date_created=datetime_orig.datetime.now())
+        Monster.objects.create(date_modified=datetime_orig.datetime.now())
+
+    def test_monster_content(self):
+        monster = Monster.objects.get(id=1)
+        expected_object_name = f'{monster.monster_name}'
+        self.assertEquals(expected_object_name, "Godzilla")
+
+        
+    def today():
+        return datetime.date.today()
