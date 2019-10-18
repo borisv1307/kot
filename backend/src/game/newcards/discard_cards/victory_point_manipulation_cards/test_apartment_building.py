c0c45ba8ff4ae75affb8d551e17b9a8f013b44b7
borisv13 <borisv13@gmail.com>
2019-10-18 17:21:25 -0400

    Testing if Sonar updates correctly

---
+++ backend/src/game/newcards/discard_cards/victory_point_manipulation_cards/test_apartment_building.py
@@ -0,0 +1,7 @@
+from backend.src.game.newcards.discard_cards.victory_point_manipulation_cards.apartment_building import \
+    ApartmentBuilding
+
+
+def test_apartment_building_adds_3_victory_points(player):
+    ApartmentBuilding().immediate_effect(player, None)
+    assert player.victory_points == 3
