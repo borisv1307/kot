cfd77a3ae1b852c7458ffdafd32098532b7ac381
borisv13 <borisv13@gmail.com>
2019-10-18 16:22:51 -0400

    Trying to remove coverage

--- backend/src/game/newcards/discard_card.py
+++ backend/src/game/newcards/discard_card.py
@@ -3,7 +3,7 @@ from abc import abstractmethod
 from backend.src.game.newcards.new_card import NewCard
 
 
-class DiscardCard(NewCard):
+class DiscardCard(NewCard):  # pragma: no cover
     @abstractmethod
     def immediate_effect(self, player_that_bought_the_card, other_players):
-        pass
+        raise NotImplementedError
