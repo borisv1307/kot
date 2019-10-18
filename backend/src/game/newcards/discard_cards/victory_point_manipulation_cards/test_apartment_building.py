17b87ffa4f9ba12c6e2a2fca773fb465a65af298
borisv13 <borisv13@gmail.com>
2019-10-18 17:27:41 -0400

    Testing if Sonar updates correctly

--- backend/src/game/newcards/discard_cards/victory_point_manipulation_cards/test_apartment_building.py
+++ backend/src/game/newcards/discard_cards/victory_point_manipulation_cards/test_apartment_building.py
@@ -5,3 +5,16 @@ from backend.src.game.newcards.discard_cards.victory_point_manipulation_cards.ap
 def test_apartment_building_adds_3_victory_points(player):
     ApartmentBuilding().immediate_effect(player, None)
     assert player.victory_points == 3
+
+
+def test_apartment_building_costs_5_energy():
+    assert ApartmentBuilding().cost == 5
+
+
+def test_apartment_building_has_effect():
+    effect = ApartmentBuilding().effect
+    assert effect and not effect.isspace()
+
+
+def test_apartment_building_has_no_footnote():
+    assert not ApartmentBuilding().footnote
