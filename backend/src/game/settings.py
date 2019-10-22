502cad220cfb0082cb715a1b9dfd0335270cf602
Oyesanmi <taiwo.oyesanmi@accenture.com>
2019-10-22 16:35:40 -0400

    added corsheaders to settings.py

--- backend/src/game/settings.py
+++ backend/src/game/settings.py
@@ -35,11 +35,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'kot',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
