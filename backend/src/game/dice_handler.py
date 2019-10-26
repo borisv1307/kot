6056f462c574952582ac7f1a8a14b101515cb344
Chris Vennel <chris@vennel.net>
2019-10-25 21:25:52 -0400

    implemented simple code review feedback

--- backend/src/game/dice_handler.py
+++ backend/src/game/dice_handler.py
@@ -3,22 +3,24 @@ import backend.src.game.constants as constants
 
 
 class DiceHandler:
-    dice_values = list()
-    __re_rolls_left = constants.DEFAULT_RE_ROLL_COUNT
+    dice_values = []
+    re_rolls_left = constants.DEFAULT_RE_ROLL_COUNT
 
     def start_turn(self):
         self.dice_values = dice.roll_many(constants.DEFAULT_DICE_COUNT)
-        self.__re_rolls_left = constants.DEFAULT_RE_ROLL_COUNT
+        self.re_rolls_left = constants.DEFAULT_RE_ROLL_COUNT
 
     def re_roll_dice(self, indexes_of_dice_to_re_roll):
-        if self.__re_rolls_left > 0:
-            if type(indexes_of_dice_to_re_roll) == int:
-                self.dice_values[indexes_of_dice_to_re_roll] = dice.roll()
+        if self.re_rolls_left <= 0:
+            return
 
-            elif type(indexes_of_dice_to_re_roll) == list:
-                for val in indexes_of_dice_to_re_roll:
-                    self.dice_values[val] = dice.roll()
-                self.__re_rolls_left -= 1
+        if type(indexes_of_dice_to_re_roll) == int:
+            self.dice_values[indexes_of_dice_to_re_roll] = dice.roll()
+
+        elif type(indexes_of_dice_to_re_roll) == list:
+            for val in indexes_of_dice_to_re_roll:
+                self.dice_values[val] = dice.roll()
+            self.re_rolls_left -= 1
 
     def add_bonus_die(self):
         self.dice_values.append(dice.roll())
