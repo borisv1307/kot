817b409fe91cf9df4b7cac05259b1fe279d18937
kaubin <kaubin@sonalysts.com>
2019-11-02 21:35:16 -0400

    bad test

--- backend/test/game/test_dice.py
+++ backend/test/game/test_dice.py
@@ -3,8 +3,3 @@ import backend.src.game.dice.dice as dice
 
 def test_roll():
     assert dice.roll() in dice.DieValue
-
-
-def test_rolls():
-    roll = dice.roll_many(2)
-    assert roll.count == 2
