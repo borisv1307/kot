c4ed68376f4d1fd955f0a63e80aeafd14d30ce14
Oyesanmi <taiwo.oyesanmi@accenture.com>
2019-11-10 15:03:45 -0500

    card updates

---
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_national_guard.py
@@ -0,0 +1,16 @@
+from game.cards.discard_cards.victory_point_manipulation_cards.national_guard import NationalGuard
+
+
+def test_jet_fighters_adds_2_victory_points(player):
+    NationalGuard().immediate_effect(player, None)
+    assert player.victory_points == 2
+
+
+def test_jet_fighters_subtracts_2_health(player):
+    player.update_health_by(10)
+    NationalGuard().immediate_effect(player, None)
+    assert player.current_health == 8
+
+
+def test_jet_fighters_costs_3_energy():
+    assert NationalGuard().cost == 3
