61d272f8a615c939060006c9e7c0387d4e8b67a9
Oyesanmi <taiwo.oyesanmi@accenture.com>
2019-11-10 15:00:06 -0500

    card updates

---
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_heal.py
@@ -0,0 +1,10 @@
+from game.cards.discard_cards.victory_point_manipulation_cards.heal import Heal
+
+
+def test_heal_adds_2_health(player):
+    Heal().immediate_effect(player, None)
+    assert player.current_health == 2
+
+
+def test_heal_costs_3_energy():
+    assert Heal().cost == 3
