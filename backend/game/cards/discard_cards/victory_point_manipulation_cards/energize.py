4b9184aef8a6faca2043d7adc5a5659987de1a6f
Oyesanmi <taiwo.oyesanmi@accenture.com>
2019-11-10 14:49:40 -0500

    card update

---
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/energize.py
@@ -0,0 +1,10 @@
+from game.cards.card import Card
+from game.cards.discard_card import DiscardCard
+
+
+class Energize(DiscardCard):
+    def __init__(self):
+        Card.__init__(self, "Drop from High Altitude", 8, "+ 9[Energy]", "")
+
+    def immediate_effect(self, player_that_bought_the_card, other_players):
+        player_that_bought_the_card.update_energy_cube_by(9)
