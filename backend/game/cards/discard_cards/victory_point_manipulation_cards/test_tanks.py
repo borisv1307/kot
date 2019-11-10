90e51165cd9066f120c2f0fc24d5dfd00d54ce29
Oyesanmi <taiwo.oyesanmi@accenture.com>
2019-11-10 15:04:40 -0500

    card updates

---
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_tanks.py
@@ -0,0 +1,16 @@
+from game.cards.discard_cards.victory_point_manipulation_cards.tanks import Tanks
+
+
+def test_jet_fighters_adds_2_victory_points(player):
+    Tanks().immediate_effect(player, None)
+    assert player.victory_points == 2
+
+
+def test_jet_fighters_subtracts_3_health(player):
+    player.update_health_by(10)
+    Tanks().immediate_effect(player, None)
+    assert player.current_health == 7
+
+
+def test_jet_fighters_costs_4_energy():
+    assert Tanks().cost == 4
