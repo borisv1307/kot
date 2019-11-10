1f1c56dbaa6d29c5578dd92a99abc0ab2bda6add
Oyesanmi <taiwo.oyesanmi@accenture.com>
2019-11-10 11:58:27 -0500

    updates for drop_from_high_altitude card

---
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/drop_from_high_altitude.py
@@ -0,0 +1,12 @@
+from game.newcards.discard_card import DiscardCard
+from game.newcards.new_card import NewCard
+
+
+class DropFromHighAltitude(DiscardCard):
+    def __init__(self):
+        NewCard.__init__(self, "Drop from High Altitude", 5, "+ 2[Star] and take control of Tokyo if you don't "
+                                                             "already control it", "")
+
+    def immediate_effect(self, player_that_bought_the_card, other_players):
+        player_that_bought_the_card.update_victory_points_by(2)
+        player_that_bought_the_card.move_to_tokyo()
