74e4f3712ec575be56499e23a150e20ec6a3fe40
Oyesanmi <taiwo.oyesanmi@accenture.com>
2019-11-10 12:40:35 -0500

    addded logic to remove other players if in tokyo

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/drop_from_high_altitude.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/drop_from_high_altitude.py
@@ -1,12 +1,16 @@
-from game.newcards.discard_card import DiscardCard
-from game.newcards.new_card import NewCard
+from game.cards.card import Card
+from game.cards.discard_card import DiscardCard
+from game.values.locations import Locations
 
 
 class DropFromHighAltitude(DiscardCard):
     def __init__(self):
-        NewCard.__init__(self, "Drop from High Altitude", 5, "+ 2[Star] and take control of Tokyo if you don't "
-                                                             "already control it", "")
+        Card.__init__(self, "Drop from High Altitude", 5, "+ 2[Star] and take control of Tokyo if you don't already "
+                                                          "control it", "")
 
     def immediate_effect(self, player_that_bought_the_card, other_players):
         player_that_bought_the_card.update_victory_points_by(2)
         player_that_bought_the_card.move_to_tokyo()
+        for other_player in other_players:
+            if other_player.location == Locations.TOKYO:
+                other_player.leave_tokyo()
--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_drop_from_high_altitude.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_drop_from_high_altitude.py
@@ -1,5 +1,5 @@
-from game.locations import Locations
-from game.newcards.discard_cards.victory_point_manipulation_cards.drop_from_high_altitude import DropFromHighAltitude
+from game.cards.discard_cards.victory_point_manipulation_cards.drop_from_high_altitude import DropFromHighAltitude
+from game.values.locations import Locations
 
 
 def test_drop_from_high_altitude_adds_2_victory_points(player):
@@ -13,3 +13,13 @@ def test_drop_from_high_altitude_costs_5_energy():
 
 def test_drop_from_high_altitude_location_tokyo():
     assert DropFromHighAltitude().location == Locations.OUTSIDE
+
+
+def test_drop_from_high_altitude_location_outside_tokyo(five_players):
+    for player in five_players:
+        if player.location == Locations.OUTSIDE:
+            player.move_to_tokyo()
+            break
+    DropFromHighAltitude().immediate_effect(None, five_players)
+    assert all(other_players.location == Locations.OUTSIDE
+               for other_players in five_players)
