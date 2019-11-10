de8295ac449aa877058f6210a5f744c033f658ec
chris <chris@vennel.net>
2019-11-10 14:17:11 -0500

    refactored  re_roll_dice() for legibility

--- backend/game/dice_handler.py
+++ backend/game/dice_handler.py
@@ -11,17 +11,15 @@ class DiceHandler:
         self.re_rolls_left = re_roll_count
 
     def re_roll_dice(self, indexes_of_dice_to_re_roll):
-        if self.re_rolls_left <= 0:
-            return
-
-        try:
-            for val in indexes_of_dice_to_re_roll:
-                self.dice_values[val] = dice.roll()
-            self.re_rolls_left -= 1
-        except IndexError:
-            raise IndexError("Re-roll index outside of dice_values range")
-        except TypeError:
-            raise TypeError("Re-roll index must be passed as list of ints")
+        if self.re_rolls_left > 0:
+            try:
+                for val in indexes_of_dice_to_re_roll:
+                    self.dice_values[val] = dice.roll()
+                self.re_rolls_left -= 1
+            except IndexError:
+                raise IndexError("Re-roll index outside of dice_values range")
+            except TypeError:
+                raise TypeError("Re-roll index must be passed as list of ints")
 
     def add_bonus_die(self):
         self.dice_values.append(dice.roll())
