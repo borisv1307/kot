7cd957aa3d85dd74f939ce8d48343e22791f09d5
Taiwo <otaiwo70@gmail.com>
2019-11-11 00:15:49 -0500

    updates to test file

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_drop_from_high_altitude.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_drop_from_high_altitude.py
@@ -2,8 +2,8 @@ from game.cards.discard_cards.victory_point_manipulation_cards.drop_from_high_al
 from game.values.locations import Locations
 
 
-def test_drop_from_high_altitude_adds_2_victory_points(player):
-    DropFromHighAltitude().immediate_effect(player, None)
+def test_drop_from_high_altitude_adds_2_victory_points(player, five_players):
+    DropFromHighAltitude().immediate_effect(player, five_players)
     assert player.victory_points == 2
 
 
@@ -11,8 +11,9 @@ def test_drop_from_high_altitude_costs_5_energy():
     assert DropFromHighAltitude().cost == 5
 
 
-def test_drop_from_high_altitude_location_tokyo():
-    assert DropFromHighAltitude().location == Locations.OUTSIDE
+def test_drop_from_high_altitude_location_tokyo(player, five_players):
+    DropFromHighAltitude().immediate_effect(player, five_players)
+    assert player.location == Locations.TOKYO
 
 
 def test_drop_from_high_altitude_location_outside_tokyo(five_players):
@@ -20,6 +21,6 @@ def test_drop_from_high_altitude_location_outside_tokyo(five_players):
         if player.location == Locations.OUTSIDE:
             player.move_to_tokyo()
             break
-    DropFromHighAltitude().immediate_effect(None, five_players)
+    DropFromHighAltitude().immediate_effect(player, five_players)
     assert all(other_players.location == Locations.OUTSIDE
                for other_players in five_players)
