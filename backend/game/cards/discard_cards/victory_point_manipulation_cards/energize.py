fa52b63ea6ffc01f57c6744c6465dc1f580ad41a
Taiwo <otaiwo70@gmail.com>
2019-11-10 19:48:19 -0500

    card updates support re-org and peer changes

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/energize.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/energize.py
@@ -7,4 +7,4 @@ class Energize(DiscardCard):
         Card.__init__(self, "Drop from High Altitude", 8, "+ 9[Energy]", "")
 
     def immediate_effect(self, player_that_bought_the_card, other_players):
-        player_that_bought_the_card.update_energy_cube_by(9)
+        player_that_bought_the_card.update_energy_by(9)
--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_energize.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_energize.py
@@ -3,7 +3,7 @@ from game.cards.discard_cards.victory_point_manipulation_cards.energize import E
 
 def test_energize_adds_9_energy_cubes(player):
     Energize().immediate_effect(player, None)
-    assert player.energy_cube == 9
+    assert player.energy == 9
 
 
 def test_energize_costs_8_energy():
