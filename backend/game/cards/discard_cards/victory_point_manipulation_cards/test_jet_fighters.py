e5bf1776217e224313f34e9d02cd148aad136ce5
Taiwo <otaiwo70@gmail.com>
2019-11-11 00:09:04 -0500

    card update

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_jet_fighters.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_jet_fighters.py
@@ -7,9 +7,9 @@ def test_jet_fighters_adds_5_victory_points(player):
 
 
 def test_jet_fighters_subtracts_4_health(player):
-    player.update_health_by(10)
+    player.current_health = 9
     JetFighters().immediate_effect(player, None)
-    assert player.current_health == 6
+    assert player.current_health == 5
 
 
 def test_jet_fighters_costs_5_energy():
