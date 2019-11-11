493e8b23a9a57247f5b5448af8e9ea7ba99fd5d2
Taiwo <otaiwo70@gmail.com>
2019-11-10 22:10:00 -0500

    card updates

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_heal.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_heal.py
@@ -2,9 +2,9 @@ from game.cards.discard_cards.victory_point_manipulation_cards.heal import Heal
 
 
 def test_heal_adds_2_health(player):
-    player.current_health = 7
+    player.current_health = 6
     Heal().immediate_effect(player, None)
-    assert player.current_health == 9
+    assert player.current_health == 8
 
 
 def test_heal_costs_3_energy():
