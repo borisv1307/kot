12b7d74ec20cd8155ba5f8ee67e06258d14623b5
Taiwo <otaiwo70@gmail.com>
2019-11-10 23:12:56 -0500

    card updates

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_high_altitude_bombing.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_high_altitude_bombing.py
@@ -1,18 +1,18 @@
 from game.cards.discard_cards.victory_point_manipulation_cards.high_altitude_bombing import HighAltitudeBombing
 
 
-def test_high_altitude_bombing_subtracts_3_health(player):
-    player.update_health_by(10)
-    HighAltitudeBombing().immediate_effect(player, None)
-    assert player.current_health == 7
+def test_high_altitude_bombing_subtracts_3_health(player, five_players):
+    player.current_health = 7
+    HighAltitudeBombing().immediate_effect(player, five_players)
+    assert player.current_health == 4
 
 
-def test_high_altitude_bombing_subtracts_3_health_other_players(five_players):
-    for player in five_players:
-        player.update_health_by(10)
-    HighAltitudeBombing().immediate_effect(None, five_players)
+def test_high_altitude_bombing_subtracts_3_health_other_players(player, five_players):
+    for other_players in five_players:
+        other_players.current_health = 7
+    HighAltitudeBombing().immediate_effect(player, five_players)
     assert all(other_players.current_health ==
-               7 for other_players in five_players)
+               4 for other_players in five_players)
 
 
 def test_high_altitude_bombing_costs_4_energy():
