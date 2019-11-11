7bb9d2cff42c780e882beeda356f78e2096f6d96
Taiwo <otaiwo70@gmail.com>
2019-11-11 00:20:42 -0500

    card update

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_nuclear_power_plant.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_nuclear_power_plant.py
@@ -7,8 +7,9 @@ def test_jet_fighters_adds_2_victory_points(player):
 
 
 def test_jet_fighters_adds_3_health(player):
+    player.current_health = 3
     NuclearPowerPlant().immediate_effect(player, None)
-    assert player.current_health == 3
+    assert player.current_health == 6
 
 
 def test_jet_fighters_costs_6_energy():
