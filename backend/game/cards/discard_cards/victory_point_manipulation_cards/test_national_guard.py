09673d90d8026575c8584e681b56b56b71f3a22f
Taiwo <otaiwo70@gmail.com>
2019-11-11 00:13:30 -0500

    card updates

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_national_guard.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_national_guard.py
@@ -7,9 +7,9 @@ def test_jet_fighters_adds_2_victory_points(player):
 
 
 def test_jet_fighters_subtracts_2_health(player):
-    player.update_health_by(10)
+    player.current_health = 8
     NationalGuard().immediate_effect(player, None)
-    assert player.current_health == 8
+    assert player.current_health == 6
 
 
 def test_jet_fighters_costs_3_energy():
