bb760f934683671c500a463d634d3702c345c411
Taiwo <otaiwo70@gmail.com>
2019-11-11 00:24:56 -0500

    card update

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_tanks.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_tanks.py
@@ -1,15 +1,15 @@
 from game.cards.discard_cards.victory_point_manipulation_cards.tanks import Tanks
 
 
-def test_jet_fighters_adds_2_victory_points(player):
+def test_jet_fighters_adds_4_victory_points(player):
     Tanks().immediate_effect(player, None)
-    assert player.victory_points == 2
+    assert player.victory_points == 4
 
 
 def test_jet_fighters_subtracts_3_health(player):
-    player.update_health_by(10)
+    player.current_health = 9
     Tanks().immediate_effect(player, None)
-    assert player.current_health == 7
+    assert player.current_health == 6
 
 
 def test_jet_fighters_costs_4_energy():
