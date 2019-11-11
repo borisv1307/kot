076749609a77b47460ee1f8e5eccaca7cdee5fa1
Taiwo <otaiwo70@gmail.com>
2019-11-10 21:25:29 -0500

    card updates

--- backend/game/cards/discard_cards/victory_point_manipulation_cards/fire_blast.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/fire_blast.py
@@ -1,5 +1,5 @@
-from game.cards.discard_card import DiscardCard
 from game.cards.card import Card
+from game.cards.discard_card import DiscardCard
 
 
 class FireBlast(DiscardCard):
@@ -8,4 +8,4 @@ class FireBlast(DiscardCard):
 
     def immediate_effect(self, player_that_bought_the_card, other_players):
         for other_player in other_players:
-            other_player.update_energy_cube_by(-2)
+            other_player.update_energy_by(-2)
--- backend/game/cards/discard_cards/victory_point_manipulation_cards/test_fire_blast.py
+++ backend/game/cards/discard_cards/victory_point_manipulation_cards/test_fire_blast.py
@@ -1,12 +1,12 @@
 from game.cards.discard_cards.victory_point_manipulation_cards.fire_blast import FireBlast
 
 
-def test_fire_blast_subtracts_2_health(five_players):
-    for player in five_players:
-        player.update_health_by(10)
-    FireBlast().immediate_effect(None, five_players)
-    assert all(other_players.current_health ==
-               8 for other_players in five_players)
+def test_fire_blast_subtracts_2_health(player, five_players):
+    for other_players in five_players:
+        other_players.update_health_by(12)
+    FireBlast().immediate_effect(player, five_players)
+    assert all(other_players.current_health == 10
+               for other_players in five_players)
 
 
 def test_fire_blast_costs_3_energy():
