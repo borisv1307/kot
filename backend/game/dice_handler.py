8b311046130ea7e279bdd0f2541734e8f9156efc
chris <chris@vennel.net>
2019-11-11 12:00:42 -0500

    simplified dice_handler

--- backend/game/dice_handler.py
+++ backend/game/dice_handler.py
@@ -13,25 +13,19 @@ class DiceHandler:
     def re_roll_dice(self, indexes_of_dice_to_re_roll):
         if self.re_rolls_left <= 0:
             return
-        elif isinstance(indexes_of_dice_to_re_roll, int):
+
+        if isinstance(indexes_of_dice_to_re_roll, int):
+            indexes_of_dice_to_re_roll = [indexes_of_dice_to_re_roll]
+
+        temp_values = self.dice_values
+        for i in indexes_of_dice_to_re_roll:
             try:
-                self.dice_values[indexes_of_dice_to_re_roll] = dice.roll()
-                self.re_rolls_left -= 1
+                temp_values[i] = dice.roll()
             except IndexError:
-                return
-        elif isinstance(indexes_of_dice_to_re_roll, list) and \
-                all(isinstance(item, int) for item in indexes_of_dice_to_re_roll):
-            dice_modified = False
-            for i in indexes_of_dice_to_re_roll:
-                try:
-                    self.dice_values[i] = dice.roll()
-                    dice_modified = True
-                except IndexError:
-                    continue
-            if dice_modified:
-                self.re_rolls_left -= 1
-        else:
-            raise TypeError("re_roll_dice needs an int or list of ints of the indexes to re-roll")
+                continue
+        if self.dice_values != temp_values:
+            self.dice_values = temp_values
+            self.re_rolls_left -= 1
 
     def add_bonus_die(self, count_to_add=1):
         for _ in range(count_to_add):
