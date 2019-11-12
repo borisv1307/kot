c2d97eada979252a6b2ff453fd0ed17fed770f37
Derek McCammond <dmm545@drexel.edu>
2019-11-11 20:57:53 -0500

    IT WORKS IT WORKS IT WORKS

--- backend/kot/routing.py
+++ backend/kot/routing.py
@@ -1,5 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import lobby.routing
 
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            lobby.routing.websocket_urlpatterns
+        )
+    ),
 })
