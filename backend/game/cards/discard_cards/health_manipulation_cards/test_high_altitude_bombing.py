7046f0df21a88f2fc7d780c384d5d3dd9e3fc3b3
Taiwo <otaiwo70@gmail.com>
2019-11-16 11:04:15 -0500

    using constants.DEFAULT_HEALTH in test

--- backend/game/cards/discard_cards/health_manipulation_cards/test_high_altitude_bombing.py
+++ backend/game/cards/discard_cards/health_manipulation_cards/test_high_altitude_bombing.py
@@ -1,15 +1,15 @@
 from game.cards.discard_cards.health_manipulation_cards.high_altitude_bombing import HighAltitudeBombing
+from game.values import constants
 
 
 def test_high_altitude_bombing_subtracts_3_health(player, five_players):
     HighAltitudeBombing().immediate_effect(player, five_players)
-    assert player.current_health == 7
+    assert player.current_health == constants.DEFAULT_HEALTH - 3
 
 
 def test_high_altitude_bombing_subtracts_3_health_other_players(player, five_players):
     HighAltitudeBombing().immediate_effect(player, five_players)
-    assert all(other_players.current_health ==
-               7 for other_players in five_players)
+    assert all(other_players.current_health == constants.DEFAULT_HEALTH - 3 for other_players in five_players)
 
 
 def test_high_altitude_bombing_costs_4_energy():
