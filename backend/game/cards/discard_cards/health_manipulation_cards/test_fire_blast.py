4796a45bb5ef75da4538dcbddd0e0f87eabff3a1
Taiwo <otaiwo70@gmail.com>
2019-11-15 20:10:44 -0500

    Using constants.DEFAULT_HEALTH as opposed to hard coded values

--- backend/game/cards/discard_cards/health_manipulation_cards/test_fire_blast.py
+++ backend/game/cards/discard_cards/health_manipulation_cards/test_fire_blast.py
@@ -1,11 +1,12 @@
 from game.cards.discard_cards.health_manipulation_cards.fire_blast import FireBlast
+from game.values import constants
 
 
 def test_fire_blast_subtracts_2_health(player, five_players):
     for other_players in five_players:
-        other_players.current_health = 8
+        other_players.current_health = constants.DEFAULT_HEALTH
     FireBlast().immediate_effect(player, five_players)
-    assert all(other_players.current_health == 6
+    assert all(other_players.current_health == 8
                for other_players in five_players)
 
 
