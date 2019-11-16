abe95d8f053794e8215602df14fe75fde1d892ad
Taiwo <otaiwo70@gmail.com>
2019-11-16 18:02:00 -0500

    slight update to test typo fix  to test name

--- backend/game/cards/discard_cards/multi_manipulation_cards/test_vast_storm.py
+++ backend/game/cards/discard_cards/multi_manipulation_cards/test_vast_storm.py
@@ -7,7 +7,7 @@ def test_vast_storm_adds_2_victory_points(player):
     assert player.victory_points == 2
 
 
-def test_vast_storm_subtracts_3_health(player):
+def test_vast_storm_subtracts_1_health(player):
     VastStorm().immediate_effect(player, None)
     assert player.current_health == constants.DEFAULT_HEALTH - 1
 
