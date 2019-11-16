908e612978de3a8272d32a4dddb976af36961a86
Taiwo <otaiwo70@gmail.com>
2019-11-16 18:08:46 -0500

    slight update to test typo fix  to test name

--- backend/game/cards/discard_cards/multi_manipulation_cards/test_national_guard.py
+++ backend/game/cards/discard_cards/multi_manipulation_cards/test_national_guard.py
@@ -2,15 +2,15 @@ from game.cards.discard_cards.multi_manipulation_cards.national_guard import Nat
 from game.values import constants
 
 
-def test_jet_fighters_adds_2_victory_points(player):
+def test_national_guard_adds_2_victory_points(player):
     NationalGuard().immediate_effect(player, None)
     assert player.victory_points == 2
 
 
-def test_jet_fighters_subtracts_2_health(player):
+def test_national_guard_subtracts_2_health(player):
     NationalGuard().immediate_effect(player, None)
     assert player.current_health == constants.DEFAULT_HEALTH - 2
 
 
-def test_jet_fighters_costs_3_energy():
+def test_national_guard_costs_3_energy():
     assert NationalGuard().cost == 3
