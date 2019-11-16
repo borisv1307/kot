02d87f0fe811d4b42ebcaa0f6f95767a1c54d05a
kaubin <kaubin@sonalysts.com>
2019-11-16 01:10:55 -0500

    cleanup dead code

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import User, Game, Dice, Message
+from .models import User, Game, Dice
 
 admin.site.register(User)
 admin.site.register(Game)
-admin.site.register(Message)
 # admin.site.register(Dice)
 # Register your models here.
--- backend/lobby/routing.py
+++ backend/lobby/routing.py
@@ -1,7 +1,5 @@
 from django.urls import re_path
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.conf.urls import url
+
 from . import consumers
 
 websocket_urlpatterns = [
