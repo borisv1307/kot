460559ddd2716a1f0fe98bd43bd9ec691e7dc359
Taiwo <otaiwo70@gmail.com>
2019-11-16 18:04:34 -0500

    slight update to test typo fix  to test name

--- backend/game/cards/discard_cards/multi_manipulation_cards/test_tanks.py
+++ backend/game/cards/discard_cards/multi_manipulation_cards/test_tanks.py
@@ -2,15 +2,15 @@ from game.cards.discard_cards.multi_manipulation_cards.tanks import Tanks
 from game.values import constants
 
 
-def test_jet_fighters_adds_4_victory_points(player):
+def test_tanks_adds_4_victory_points(player):
     Tanks().immediate_effect(player, None)
     assert player.victory_points == 4
 
 
-def test_jet_fighters_subtracts_3_health(player):
+def test_tanks_subtracts_3_health(player):
     Tanks().immediate_effect(player, None)
     assert player.current_health == constants.DEFAULT_HEALTH - 3
 
 
-def test_jet_fighters_costs_4_energy():
+def test_tanks_costs_4_energy():
     assert Tanks().cost == 4
