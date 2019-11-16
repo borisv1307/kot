538a80d056706f79e38f12d29929dd29566eaf55
Taiwo <otaiwo70@gmail.com>
2019-11-15 20:29:38 -0500

    remove pre sets in test

--- backend/game/cards/discard_cards/health_manipulation_cards/test_high_altitude_bombing.py
+++ backend/game/cards/discard_cards/health_manipulation_cards/test_high_altitude_bombing.py
@@ -2,17 +2,14 @@ from game.cards.discard_cards.health_manipulation_cards.high_altitude_bombing im
 
 
 def test_high_altitude_bombing_subtracts_3_health(player, five_players):
-    player.current_health = 7
     HighAltitudeBombing().immediate_effect(player, five_players)
-    assert player.current_health == 4
+    assert player.current_health == 7
 
 
 def test_high_altitude_bombing_subtracts_3_health_other_players(player, five_players):
-    for other_players in five_players:
-        other_players.current_health = 7
     HighAltitudeBombing().immediate_effect(player, five_players)
     assert all(other_players.current_health ==
-               4 for other_players in five_players)
+               7 for other_players in five_players)
 
 
 def test_high_altitude_bombing_costs_4_energy():
