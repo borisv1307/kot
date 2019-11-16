c42131526535c7bb24c4c51216b9f14314450408
Taiwo <otaiwo70@gmail.com>
2019-11-15 20:16:26 -0500

    using constants.DEFAULT_HEALTH as oppossed to hard coded values

--- backend/game/cards/discard_cards/health_manipulation_cards/test_heal.py
+++ backend/game/cards/discard_cards/health_manipulation_cards/test_heal.py
@@ -1,10 +1,11 @@
 from game.cards.discard_cards.health_manipulation_cards.heal import Heal
+from game.values import constants
 
 
 def test_heal_adds_2_health(player):
-    player.current_health = 6
+    player.current_health = constants.DEFAULT_HEALTH
     Heal().immediate_effect(player, None)
-    assert player.current_health == 8
+    assert player.current_health == 10
 
 
 def test_heal_costs_3_energy():
