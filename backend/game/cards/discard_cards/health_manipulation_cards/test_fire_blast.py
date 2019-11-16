ccd8e26fbe2212a82754bfc7e0323ba7fe6bf972
Taiwo <otaiwo70@gmail.com>
2019-11-15 20:26:24 -0500

    decided to remove test pre sets

--- backend/game/cards/discard_cards/health_manipulation_cards/test_fire_blast.py
+++ backend/game/cards/discard_cards/health_manipulation_cards/test_fire_blast.py
@@ -1,10 +1,7 @@
 from game.cards.discard_cards.health_manipulation_cards.fire_blast import FireBlast
-from game.values import constants
 
 
 def test_fire_blast_subtracts_2_health(player, five_players):
-    for other_players in five_players:
-        other_players.current_health = constants.DEFAULT_HEALTH
     FireBlast().immediate_effect(player, five_players)
     assert all(other_players.current_health == 8
                for other_players in five_players)
