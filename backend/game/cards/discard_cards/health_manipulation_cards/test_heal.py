3cce4221be8230726e2c2ca4e242d6d9aea18ee5
Taiwo <otaiwo70@gmail.com>
2019-11-15 20:23:09 -0500

    decided to remove pre sets

--- backend/game/cards/discard_cards/health_manipulation_cards/test_heal.py
+++ backend/game/cards/discard_cards/health_manipulation_cards/test_heal.py
@@ -1,9 +1,7 @@
 from game.cards.discard_cards.health_manipulation_cards.heal import Heal
-from game.values import constants
 
 
 def test_heal_adds_2_health(player):
-    player.current_health = constants.DEFAULT_HEALTH
     Heal().immediate_effect(player, None)
     assert player.current_health == 10
 
