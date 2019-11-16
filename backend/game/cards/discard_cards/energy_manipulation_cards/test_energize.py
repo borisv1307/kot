f55217b7f14f139fd3d7417b253b9d3b400c14aa
Taiwo <otaiwo70@gmail.com>
2019-11-16 11:02:32 -0500

    using constants.DEFAULT_HEALTH in test

--- backend/game/cards/discard_cards/energy_manipulation_cards/test_energize.py
+++ backend/game/cards/discard_cards/energy_manipulation_cards/test_energize.py
@@ -1,9 +1,10 @@
 from game.cards.discard_cards.energy_manipulation_cards.energize import Energize
+from game.values import constants
 
 
 def test_energize_adds_9_energy_cubes(player):
     Energize().immediate_effect(player, None)
-    assert player.energy == 9
+    assert player.energy == constants.DEFAULT_ENERGY_CUBE + 9
 
 
 def test_energize_costs_8_energy():
