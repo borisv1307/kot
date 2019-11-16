65020d89d460f9932926be097630b897b681dd26
Taiwo <otaiwo70@gmail.com>
2019-11-16 11:04:55 -0500

    using constants.DEFAULT_HEALTH in test

--- backend/game/cards/discard_cards/multi_manipulation_cards/test_jet_fighters.py
+++ backend/game/cards/discard_cards/multi_manipulation_cards/test_jet_fighters.py
@@ -1,4 +1,5 @@
 from game.cards.discard_cards.multi_manipulation_cards.jet_fighters import JetFighters
+from game.values import constants
 
 
 def test_jet_fighters_adds_5_victory_points(player):
@@ -8,7 +9,7 @@ def test_jet_fighters_adds_5_victory_points(player):
 
 def test_jet_fighters_subtracts_4_health(player):
     JetFighters().immediate_effect(player, None)
-    assert player.current_health == 6
+    assert player.current_health == constants.DEFAULT_HEALTH - 4
 
 
 def test_jet_fighters_costs_5_energy():
