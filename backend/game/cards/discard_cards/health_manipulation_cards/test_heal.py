1e8e7e4f23e4729d442716ebf5fda26320fb7f17
Taiwo <otaiwo70@gmail.com>
2019-11-16 17:58:09 -0500

    slight update to accommodate test using constants.DEFAULT_HEALTH

--- backend/game/cards/discard_cards/health_manipulation_cards/test_heal.py
+++ backend/game/cards/discard_cards/health_manipulation_cards/test_heal.py
@@ -3,6 +3,7 @@ from game.values import constants
 
 
 def test_heal_adds_2_health(player):
+    player.current_health = player.current_health - 2
     Heal().immediate_effect(player, None)
     assert player.current_health == constants.DEFAULT_HEALTH
 
