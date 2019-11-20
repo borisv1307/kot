2908262db249dfa1633876717bdf7a5aaf989afa
Gerardo Faia <gpfaia@users.noreply.github.com>
2019-11-19 21:11:32 -0500

    Changed win conditions Add function to keep track of game status

--- backend/game/engine/board.py
+++ backend/game/engine/board.py
@@ -17,7 +17,9 @@ class BoardGame():
         self.STATUS = status.ACTIVE
 
     def check_if_winner(self, player):
-        if player.victory_points == constants.VICTORY_POINTS_TO_WIN:
+        if player.victory_points == constants.VICTORY_POINTS_TO_WIN or all(
+                alive_player == player for alive_player
+                in self.players.get_alive_player()):
             self.WINNER = player
             self.end_game()
 
@@ -28,8 +30,14 @@ class BoardGame():
         # roll dice & dice_resoultion method
         pass
 
+    def is_game_active(self):
+        if self.STATUS == status.ACTIVE:
+            return True
+        else:
+            return False
+
     def play_game(self):
-        while self.STATUS == status.ACTIVE:
+        while self.is_game_active():
             current_player = self.players.get_current_player()
             self.turn(current_player)
             self.check_if_winner(current_player)
