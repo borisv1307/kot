c74bf2e04b3e106490970b96a1385758a0ef785a
Gerardo Faia <gpfaia@users.noreply.github.com>
2019-11-23 17:12:16 -0500

    Comment removal explict instance checking Cleaned up code

--- backend/game/engine/board.py
+++ backend/game/engine/board.py
@@ -9,33 +9,30 @@ class BoardGame():
 
     def __init__(self):
         self.players = GamePlayers()
-        self.STATUS = status.SETUP
-        self.WINNER = None
+        self.status = status.SETUP
+        self.winner = None
 
     def start_game(self):
         self.players.set_player_order()
-        self.STATUS = status.ACTIVE
+        self.status = status.ACTIVE
 
     def check_if_winner(self, potential_winner):
         if (potential_winner.victory_points == constants.VICTORY_POINTS_TO_WIN or
                 players.if_last_player_alive(potential_winner)):
-            self.WINNER = potential_winner
+            self.winner = potential_winner
             self.end_game()
 
     def end_game(self):
-        self.STATUS = status.COMPLETED
+        self.status = status.COMPLETED
 
     def turn(self, active_player):
-        if active_player == self.players.get_current_player():
-            self.turn_actions(active_player)
-            self.check_if_winner(active_player)
-        else:
-            # TODO player object should have a name property here
+        if active_player != self.players.get_current_player():
             raise Exception("It is not %s's turn" % (id(active_player)))
+        self.turn_actions(active_player)
+        self.check_if_winner(active_player)
 
     def is_game_active(self):
-        return self.STATUS == status.ACTIVE
+        return self.status == status.ACTIVE
 
     def turn_actions(self, turn_player):
-        # roll dice & dice_resoultion method
         pass
--- backend/game/engine/player_queue.py
+++ backend/game/engine/player_queue.py
@@ -6,17 +6,16 @@ from game.player.player import Player
 class GamePlayers():
 
     def __init__(self):
-        self.all_players = []
+        self.players = []
         self.current_player = None
 
     def _add_player_to_game(self, new_player):
-        if isinstance(new_player, Player):
-            self.all_players.append(new_player)
-        else:
+        if not isinstance(new_player, Player):
             raise TypeError("Object passed in was not an instance of Player")
+        self.players.append(new_player)
 
     def set_player_order(self):
-        self.player_cycle = itertools.cycle(self.all_players)
+        self.player_cycle = itertools.cycle(self.players)
 
     def get_current_player(self):
         if not self.current_player:
@@ -27,10 +26,10 @@ class GamePlayers():
         self.current_player = self._cycle_next_alive_player()
 
     def get_alive_players(self):
-        return [player for player in self.all_players if player.is_alive]
+        return [player for player in self.players if player.is_alive]
 
     def get_dead_players(self):
-        return [player for player in self.all_players if not player.is_alive]
+        return [player for player in self.players if not player.is_alive]
 
     def get_all_alive_players_minus_current_player(self):
         return [player for player in self.get_alive_players() if
@@ -40,8 +39,8 @@ class GamePlayers():
         return [possible_last_player] == self.players.get_alive_players()
 
     def _cycle_next_alive_player(self):
+        if not self.get_alive_players():
+            raise Exception("There are no alive players left")
         for player in self.player_cycle:
             if player.is_alive:
                 return player
-            elif not self.get_alive_players():
-                return None
