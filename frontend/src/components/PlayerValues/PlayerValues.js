1fb77cfbf0699f6aafb3af6f47afc9f57f970965
Lakshmi <vlakshmimaguluri@gmail.com>
2019-11-24 18:25:57 -0500

    Added energy and styled the component

--- frontend/src/components/PlayerValues/PlayerValues.js
+++ frontend/src/components/PlayerValues/PlayerValues.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Heart from './../Images/Heart.gif'
+import Star from './../Images/Star.png'
+import Energy from './../Images/Energy.png'
 
 import "./PlayerValues.css";
 
@@ -6,18 +9,21 @@ class PlayerValues extends React.Component {
     state = {
         victory_points: 0,
         health: 10,
+        energy: 0,
         position: 'Out'
     }
 
     render() {
+
         return (
             <div>
                 <div className="player-border">
                     <h2>Player A </h2>
                     <div className="player-values">
-                        <p>Victory Points: <span className="badge badge-light">{this.state.victory_points}</span></p>
-                        <p>Health: <span className="badge badge-light">{this.state.health}</span></p>
-                        <p>In/Out of Tokyo: <span >{this.state.position}</span></p>
+                        <p>Victory Points: <span className="badge badge-light">{this.state.victory_points}</span><img src={Star}></img></p>
+                        <p>Health: <span className="badge badge-light">{this.state.health}</span><img src={Heart}></img></p>
+                        <p>Energy: <span className="badge badge-light">{this.state.energy}</span><img src={Energy}></img></p>
+                        <p>In/Out of Tokyo: <span className="badge badge-warning">{this.state.position}</span></p>
                     </div>
                 </div>
             </div>
