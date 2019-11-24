241674253a91f82ff0a968cc16bcac52c0f4f97a
Lakshmi <vlakshmimaguluri@gmail.com>
2019-11-24 16:40:48 -0500

    Added player values for one player

--- frontend/src/components/PlayerValues/PlayerValues.js
+++ frontend/src/components/PlayerValues/PlayerValues.js
@@ -2,15 +2,27 @@ import React from 'react';
 
 import "./PlayerValues.css";
 
-const PlayerValues = (props) => {
+class PlayerValues extends React.Component {
+    state = {
+        victory_points: 0,
+        health: 10,
+        position: 'Out'
+    }
 
-    return (
-        <div>
-            {
-                <h2>Display player stats</h2>
-            }
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                <div className="player-border">
+                    <h2>Player A </h2>
+                    <div className="player-values">
+                        <p>Victory Points: <span className="badge badge-light">{this.state.victory_points}</span></p>
+                        <p>Health: <span className="badge badge-light">{this.state.health}</span></p>
+                        <p>In/Out of Tokyo: <span >{this.state.position}</span></p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default PlayerValues;
