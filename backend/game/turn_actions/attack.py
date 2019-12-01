5d3c129f68e73d270bf8fbce6d46bae7331ddcb1
Gerardo Faia <gpfaia@users.noreply.github.com>
2019-12-01 11:17:43 -0500

    Dice resoultion is now individual functions Cleaned up some code

--- backend/game/turn_actions/attack.py
+++ backend/game/turn_actions/attack.py
@@ -4,9 +4,10 @@ from game.turn_actions.player_movement import take_tokyo_on_kill
 
 def is_attackable(attacking_player, other_player):
     if attacking_player.location != other_player.location:
-        if Locations.OUTSIDE in {attacking_player.location, other_player.location}:
+        if Locations.OUTSIDE in {attacking_player.location,
+                                 other_player.location}:
             return True
-
+    return False
 
 
 def get_attackable_players(attacking_player, other_players):
@@ -20,5 +21,5 @@ def get_attackable_players(attacking_player, other_players):
 def attack_players(attacking_player, attackable_players, attack_value=1):
     for attacked_player in attackable_players:
         attacked_player.update_health_by(attack_value * -1)
-        if attacked_player.is_alive is False:
+        if not attacked_player.is_alive:
             take_tokyo_on_kill(attacking_player, attacked_player)
--- backend/game/turn_actions/player_movement.py
+++ backend/game/turn_actions/player_movement.py
@@ -1,7 +1,7 @@
 from game.values.locations import Locations
 
 
-def tokyo_is_empty(attacking_player, other_players):
+def _is_tokyo_empty(attacking_player, other_players):
     if attacking_player.location != Locations.OUTSIDE:
         return False
     for player in other_players:
@@ -11,18 +11,18 @@ def tokyo_is_empty(attacking_player, other_players):
 
 
 def move_to_tokyo_if_empty(attacking_player, other_players):
-    if tokyo_is_empty(attacking_player, other_players):
+    if _is_tokyo_empty(attacking_player, other_players):
         attacking_player.move_to_tokyo()
         attacking_player.update_victory_points_by(1)
 
 
-def take_tokyo_on_kill(attacking_player, murdered_player):
-    if attacking_player.location == Locations.OUTSIDE and murdered_player.location != Locations.OUTSIDE:
-        yield_tokyo(murdered_player, attacking_player)
+def take_tokyo_on_kill(attacking_player, attacked_player):
+    if (attacking_player.location == Locations.OUTSIDE and attacked_player.
+            location != Locations.OUTSIDE):
+        yield_tokyo(attacked_player, attacking_player)
 
 
 def yield_tokyo(yielding_player, attacking_player):
     yielding_player.leave_tokyo()
     attacking_player.move_to_tokyo()
     attacking_player.update_victory_points_by(1)
-
