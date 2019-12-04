dbc5ee8ccaf8d2786739872c51d5295cbf14d4fb
Sam Prasad <samridhprasad@gmail.com>
2019-12-04 01:06:59 -0500

    Dummy keep card reducer-store interactions work

--- backend/game/flux/keep_card_reducer.py
+++ backend/game/flux/keep_card_reducer.py
@@ -1,7 +1,7 @@
 import pydux
 
 
-def counter(state, action):
+def keep_card_reducer(state, action):
     if state is None:
         state = 0
     if action is None:
@@ -15,16 +15,16 @@ def counter(state, action):
 
 # Create a Redux store holding the state of your app.
 # Its API is { subscribe, dispatch, get_state }.
-store = pydux.create_store(counter)
+keep_card_store = pydux.create_store(keep_card_reducer)
 
 # You can use subscribe() to update the UI in response to state changes.
-store.subscribe(lambda: print(store.get_state()))
+keep_card_store.subscribe(lambda: print(keep_card_store.get_state()))
 
 # The only way to mutate the internal state is to dispatch an action.
 # The actions can be serialized, logged or stored and later replayed.
-store.dispatch({'type': 'INCREMENT'})
+keep_card_store.dispatch({'type': 'INCREMENT'})
 # 1
-store.dispatch({'type': 'INCREMENT'})
+keep_card_store.dispatch({'type': 'INCREMENT'})
 # 2
-store.dispatch({'type': 'DECREMENT'})
+keep_card_store.dispatch({'type': 'DECREMENT'})
 # 1
