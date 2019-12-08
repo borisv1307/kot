922c02fa22fefaef60030d3e7c05bc830c9274cc
cameronbaffuto <cb3426@drexel.edu>
2019-12-07 23:21:10 -0500

    added a register user form on the login page. it redirects to the lobby at the moment but will be connected when user authentication and account creation is ready

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 import "./Login.css";
-
 import Form from "react-bootstrap/Form";
+import RegisterUser from "./RegisterUser";
 
 export default class LoginLayout extends Component {
 
     constructor(props) {
         super(props);
 
-        let user = 'Guest_1234';
+        let user = '';
+        let password = '';
         let room = 'Room_1234';
         if (props.location && props.location.state) {
             user = 'Guest_' + Math.floor((Math.random() * 10000) + 1)
@@ -18,13 +19,21 @@ export default class LoginLayout extends Component {
 
         this.state = {
             redirect: false,
-            username: user
+            username: user,
+            userPassword: password
         };
     }
 
     usernameChangeHandler = (event) => {
         this.setState({
             username: event.target.value,
+            redirect: false
+        })
+    }
+
+    passwordChangeHandler = (event) => {
+        this.setState({
+            userPassword: event.target.value,
             redirect: false
         })
     }
@@ -68,12 +77,19 @@ export default class LoginLayout extends Component {
                         <br />
                         <div className="login">
                             <Form class="login">
+                                <h5>Login to Join a Game</h5>
                                 <Form.Group href="/lobby" onSubmit={() => this.props.onSubmit(this.state.username)}>
-                                    <Form.Label>Login to Join a Game</Form.Label>
+                                    <Form.Label>Username</Form.Label>
                                     <Form.Control type="text"
                                         onChange={this.usernameChangeHandler}
                                         placeholder="Enter your Username"
                                         value={this.state.username}
+                                        required />
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password"
+                                        onChange={this.passwordChangeHandler}
+                                        placeholder="Enter your Password"
+                                        value={this.state.password}
                                         required />
                                     {this.renderRedirect()}
                                 </Form.Group>
@@ -87,7 +103,7 @@ export default class LoginLayout extends Component {
                         </a> */}
                     </div>
                     <div className="col-sm">
-
+                        <RegisterUser/>
                     </div>
                 </div>
             </div>
---
+++ frontend/src/containers/RegisterUser.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+import "./Login.css";
+import Form from "react-bootstrap/Form";
+
+class RegisterUser extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      first_name:'',
+      last_name:'',
+      email:'',
+      password:''
+    }
+  }
+
+  setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
+    }
+
+    renderRedirect = () => {
+        const user = this.state.username;
+        if (this.state.redirect) {
+            return <Redirect to={{
+                pathname: '/lobby',
+                state: { username: user }
+            }} />
+        }
+    }
+
+
+  render() {
+    return (
+      <div>
+        <Form>
+            <h2>Register</h2>
+            <Form.Group href="/lobby" onSubmit={() => this.props.onSubmit(this.state.username)}>
+            <Form.Label>First Name</Form.Label>
+           <Form.Control
+             placeholder="Enter your First Name"
+             floatingLabelText="First Name"
+             onChange = {(event,newValue) => this.setState({first_name:newValue})}
+             />
+             <Form.Label>Last Name</Form.Label>
+           <Form.Control
+             placeholder="Enter your Last Name"
+             floatingLabelText="Last Name"
+             onChange = {(event,newValue) => this.setState({last_name:newValue})}
+             />
+             <Form.Label>Email</Form.Label>
+           <Form.Control
+             placeholder="Enter your Email"
+             type="email"
+             floatingLabelText="Email"
+             onChange = {(event,newValue) => this.setState({email:newValue})}
+             />
+             <Form.Label>Password</Form.Label>
+           <Form.Control
+             type = "password"
+             placeholder="Enter your Password"
+             floatingLabelText="Password"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+             {this.renderRedirect()}
+            </Form.Group>
+            <Button variant="btn btn-secondary" onClick={this.setRedirect} className="submit" type="submit" value="Submit">Login</Button>
+         </Form>
+      </div>
+    );
+  }
+}
+export default RegisterUser
