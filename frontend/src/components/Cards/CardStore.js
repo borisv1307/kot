6f17af2d416c15e5d36c0432ec323ace7aaded81
Lakshmi <vlakshmimaguluri@gmail.com>
2020-01-22 04:03:46 -0500

    Testing out card display with map

--- frontend/src/components/Cards/CardStore.js
+++ frontend/src/components/Cards/CardStore.js
@@ -5,27 +5,32 @@ import Button from 'react-bootstrap/Button';
 import GameInstance from "../../services/gameService";
 
 class CardStore extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-        this.handleChange = this.handleChange.bind(this);
-        this.clearSelected = this.clearSelected.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.clearSelected = this.clearSelected.bind(this);
 
-        this.state = {
-            username: props.currentUser,
-            gameRoom: props.currentRoom,
-            value: [],
-            selectedCard: [],
-            cardName: 'Card Name',
-            cardCost: 3,
-            cardType: 'keep',
-            cardEffect: 'lose two hearts, gain 4 energy',
-            cardFootnote: 'footnote description',
+    this.state = {
+      index: [3],
+      username: props.currentUser,
+      gameRoom: props.currentRoom,
+      value: [],
+      selectedCard: [],
+      card: [
+        {
+          name: 'Card Name',
+          cost: 3,
+          type: 'keep',
+          effect: 'lose two hearts, gain 4 energy',
+          footnote: 'footnote description'
+        }
+      ]
 
-        };
-    }
+    };
+  }
 
-    componentDidMount() {
+  componentDidMount() {
     // ... do something with fetchedData e.g. set the data
   }
 
@@ -46,61 +51,38 @@ class CardStore extends React.Component {
     }
   }
 
-    handleChange(e) {
-        this.setState({ value: e });
-        this.props.callbackSendCardSelectionOut(e);
-    }
+  handleChange(e) {
+    this.setState({ value: e });
+    this.props.callbackSendCardSelectionOut(e);
+  }
 
-    clearSelected(e) {
-        this.setState({ value: [] });
-    }
+  clearSelected(e) {
+    this.setState({ value: [] });
+  }
 
 
-    render() {
-        return (
-            <container className="card">
-            <div className='row'>
-             <div className='col-sm'>
-            <div id='card'>
-            <Card>
-                <Card.Body>
-                <Card.Title>{this.state.cardName}</Card.Title>
-                 <Card.Subtitle className="mb-2 text-muted">{this.state.cardCost}</Card.Subtitle>
-                    <Card.Text>{this.state.cardType}</Card.Text>
-                    <Card.Text>{this.state.cardEffect}</Card.Text>
-                    <Card.Text>{this.state.cardFootnote}</Card.Text>
-                    </Card.Body>
-            </Card>
+  render() {
+    return (
+      <div>
+        <Card className="card">
+          {
+            this.state.card.map((card, key) => (
+              <div key={key}>
+                <div>{card.name}</div>
+                <div>{card.cost}</div>
+                <div>{card.type}</div>
+                <div>{card.footnote}</div>
+              </div>
+            )
 
-            <Card>
-                <Card.Body>
-                <Card.Title>{this.state.cardName}</Card.Title>
-                 <Card.Subtitle className="mb-2 text-muted">{this.state.cardCost}</Card.Subtitle>
-                    <Card.Text>{this.state.cardType}</Card.Text>
-                    <Card.Text>{this.state.cardEffect}</Card.Text>
-                    <Card.Text>{this.state.cardFootnote}</Card.Text>
-                    </Card.Body>
-            </Card>
+            )}
+        </Card>
 
-            <Card>
-                <Card.Body>
-                <Card.Title>{this.state.cardName}</Card.Title>
-                 <Card.Subtitle className="mb-2 text-muted">{this.state.cardCost}</Card.Subtitle>
-                    <Card.Text>{this.state.cardType}</Card.Text>
-                    <Card.Text>{this.state.cardEffect}</Card.Text>
-                    <Card.Text>{this.state.cardFootnote}</Card.Text>
-                    </Card.Body>
-            </Card>
-            </div>
-                <Button onClick={this.setRedirect} className="btn btn-secondary">Card Store{this.state.CardStore}</Button>
-                 &nbsp;&nbsp;&nbsp;
-                <Button onClick={this.shuffleCards} className="btn btn-secondary">Shuffle Cards{this.state.allowShuffleCards}</Button>
-            </div>
-            </div>
-            </container>
-
-        );
-    }
+        <Button onClick={this.setRedirect} className="btn btn-secondary">Card Store{this.state.CardStore}</Button>
+        &nbsp;&nbsp;
+        <Button onClick={this.shuffleCards} className="btn btn-secondary">Shuffle Cards{this.state.allowShuffleCards}</Button>
+      </div>
+    );
+  }
 }
-
 export default CardStore;
