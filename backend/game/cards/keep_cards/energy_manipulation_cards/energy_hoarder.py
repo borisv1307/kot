00f78f474450c17f6080cccbcd0db234f6159c2c
Chris Vennel <chris@vennel.net>
2020-01-25 21:16:11 -0500

    updated energy_hoarder to use existing methods, attached to turn logic

--- backend/game/cards/keep_cards/energy_manipulation_cards/energy_hoarder.py
+++ backend/game/cards/keep_cards/energy_manipulation_cards/energy_hoarder.py
@@ -8,10 +8,6 @@ class EnergyHoarder(KeepCard):
         Card.__init__(self, "Energy Hoarder", 3,
                       "You gain 1[Star] for every 6[Energy] you have at the end of your turn.", "")
 
-    def immediate_effect(self, player_that_bought_the_card, other_players):
-        card = EnergyHoarder()
-        player_that_bought_the_card.add_card(card)
-
     def special_effect(self, player_that_bought_the_card, other_players):
         num_stars = player_that_bought_the_card.energy // constants.ENERGY_HOARDER_DIVIDER
         player_that_bought_the_card.update_victory_points_by(num_stars)
--- backend/game/cards/keep_cards/energy_manipulation_cards/test_energy_hoarder.py
+++ backend/game/cards/keep_cards/energy_manipulation_cards/test_energy_hoarder.py
@@ -3,7 +3,7 @@ from game.cards.keep_cards.energy_manipulation_cards.energy_hoarder import Energ
 
 def test_energy_hoarder_player_add_card(player):
     EnergyHoarder().immediate_effect(player, None)
-    assert player.card_on_hand.get("Energy Hoarder")
+    assert player.has_instance_of_card(EnergyHoarder())
 
 
 def test_energy_hoarder_costs_4_energy():
