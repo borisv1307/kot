70254ea774b9cf8060ba80cd24ac21274b6c2f53
Chris Vennel <chris@vennel.net>
2020-01-25 20:56:44 -0500

    removed unnecessary method

--- backend/game/cards/keep_cards/energy_manipulation_cards/solar_powered.py
+++ backend/game/cards/keep_cards/energy_manipulation_cards/solar_powered.py
@@ -6,10 +6,6 @@ class SolarPowered(KeepCard):
     def __init__(self):
         Card.__init__(self, "Solar Powered", 4, "At the end of your turn gain 1[Energy] if you have no [Energy].", "")
 
-    def immediate_effect(self, player_that_bought_the_card, other_players):
-        card = SolarPowered()
-        player_that_bought_the_card.add_card(card)
-
     def special_effect(self, player_that_bought_the_card, other_players):
         if player_that_bought_the_card.energy == 0:
             player_that_bought_the_card.update_energy_by(1)
