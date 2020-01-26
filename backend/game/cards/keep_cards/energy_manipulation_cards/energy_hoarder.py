6c40ea7a8551793777dae7dac7f0342c55af333d
Chris Vennel <chris@vennel.net>
2020-01-25 21:24:04 -0500

    updated to use super

--- backend/game/cards/keep_cards/energy_manipulation_cards/energy_hoarder.py
+++ backend/game/cards/keep_cards/energy_manipulation_cards/energy_hoarder.py
@@ -1,12 +1,11 @@
-from game.cards.card import Card
 from game.cards.keep_card import KeepCard
 from game.values import constants
 
 
 class EnergyHoarder(KeepCard):
     def __init__(self):
-        Card.__init__(self, "Energy Hoarder", 3,
-                      "You gain 1[Star] for every 6[Energy] you have at the end of your turn.", "")
+        super().__init__("Energy Hoarder", 3,
+                         "You gain 1[Star] for every 6[Energy] you have at the end of your turn.")
 
     def special_effect(self, player_that_bought_the_card, other_players):
         num_stars = player_that_bought_the_card.energy // constants.ENERGY_HOARDER_DIVIDER
