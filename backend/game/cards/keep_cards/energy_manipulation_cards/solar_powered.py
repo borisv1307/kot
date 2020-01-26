6222b2a38069b217d422178a8b89ee6c90bad3df
Chris Vennel <chris@vennel.net>
2020-01-25 21:26:30 -0500

    updated to use super

--- backend/game/cards/keep_cards/energy_manipulation_cards/solar_powered.py
+++ backend/game/cards/keep_cards/energy_manipulation_cards/solar_powered.py
@@ -1,10 +1,9 @@
-from game.cards.card import Card
 from game.cards.keep_card import KeepCard
 
 
 class SolarPowered(KeepCard):
     def __init__(self):
-        Card.__init__(self, "Solar Powered", 4, "At the end of your turn gain 1[Energy] if you have no [Energy].", "")
+        super().__init__("Solar Powered", 4, "At the end of your turn gain 1[Energy] if you have no [Energy].")
 
     def special_effect(self, player_that_bought_the_card, other_players):
         if player_that_bought_the_card.energy == 0:
