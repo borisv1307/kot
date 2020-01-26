ce854e3119b14b5d4f9201bfea3059e7a6219700
kaubin <kaubin@sonalysts.com>
2020-01-25 23:52:29 -0500

    removing internal add-remove from player, bad coupling - merge master

--- backend/game/cards/keep_card.py
+++ backend/game/cards/keep_card.py
@@ -7,11 +7,11 @@ class KeepCard(Card):
         super().__init__(name, cost, effect)
 
     def immediate_effect(self, player_that_bought_the_card, other_players):
-        raise NotImplementedError
+        pass
 
     @abstractmethod
     def special_effect(self, player_that_bought_the_card, other_players):
         raise NotImplementedError
 
     def discard_effect(self, player_that_bought_the_card, other_players):
-        raise NotImplementedError
+        pass
