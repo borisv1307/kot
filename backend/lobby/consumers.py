f7f2a0f1affccfb75187c171d0964b25ae031de1
Lakshmi <vlakshmimaguluri@gmail.com>
2020-01-27 18:17:40 -0500

    Modified handler in consumers.py

--- backend/lobby/consumers.py
+++ backend/lobby/consumers.py
@@ -6,9 +6,6 @@ import django
 import os
 
 from game.models import User
-
-from channels.generic.websocket import WebsocketConsumer
-
 from backend.game.engine.board import BoardGame
 
 
@@ -107,11 +104,15 @@ class GameConsumer(WebsocketConsumer):
 
         self.send_server_response_to_client(username, room, mud_gamelog_input)
 
-    def randomcards_deck_handler(self, data):
+    def selected_card_handler(self, data):
         username = data['user']
         room = data['room']
-        cards_required = data['payload']
-        self.send_server_response_to_client('GAMECONSOLE_DECISION', username, room, cards)
+
+        print("selected card handler")
+
+        payload = data['payload']
+
+        self.send_server_response_to_client(username, room, payload)
 
     def get_the_three_store_cards(self):
         card_market = self.master_game_controller.deck_handler.store
@@ -121,17 +122,18 @@ class GameConsumer(WebsocketConsumer):
         card3 = card_market[2]
         return card_market
 
-# This test is temporary to prove the GameConsumer has access to the card store
-def test_get_store_cards():
-    game = GameConsumer(WebsocketConsumer("test"))
-    three_cards = game.get_the_three_store_cards()
-    print(
-        "\n\tCARD 1:{}\n\tCARD 2:{}\n\tCARD 3:{}".format(three_cards[0].name, three_cards[1].name, three_cards[2].name))
-    assert len(three_cards) == 3
+    # This test is temporary to prove the GameConsumer has access to the card store
+    def test_get_store_cards():
+        game = GameConsumer(WebsocketConsumer("test"))
+        three_cards = game.get_the_three_store_cards()
+        print(
+            "\n\tCARD 1:{}\n\tCARD 2:{}\n\tCARD 3:{}".format(three_cards[0].name, three_cards[1].name, three_cards[2].name))
+        assert len(three_cards) == 3
     
     commands = {
         'init_user_request': init_chat_handler,
         'gamelog_send_request': gamelog_send_handler,
-        'selected_dice_request': selected_dice_handler
-        'randomcards_send_request': randomcards_deck_handler
+        'selected_dice_request': selected_dice_handler,
+        'send_cards_request': get_the_three_store_cards
+
     }
