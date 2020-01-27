9eb297563190bb0e10670f936da7cc3efb595093
Chris Vennel <chris@vennel.net>
2020-01-26 21:34:34 -0500

    addressed comments

--- backend/game/cards/card.py
+++ backend/game/cards/card.py
@@ -7,7 +7,3 @@ class Card(ABC):
         self.cost = cost
         self.effect = effect
         self.footnote = footnote
-
-    # Card is a user defined object. Provided comparison context other than object reference.
-    def __eq__(self, other):
-        return self is other or self.name == other.name
--- backend/game/cards/keep_cards/health_manipulation_cards/even_bigger.py
+++ backend/game/cards/keep_cards/health_manipulation_cards/even_bigger.py
@@ -1,12 +1,10 @@
-from game.cards.card import Card
 from game.cards.keep_card import KeepCard
 
 
 class EvenBigger(KeepCard):
     def __init__(self):
-        super(KeepCard, self).__init__("Even Bigger", 4,
-                                       "Your maximum [Heart] is increased by 2. Gain 2[Heart] when you get this card",
-                                       "")
+        super().__init__("Even Bigger", 4, "Your maximum [Heart] is increased by 2. Gain 2[Heart] when you get this "
+                                           "card")
 
     def immediate_effect(self, player_that_bought_the_card, other_players):
         player_that_bought_the_card.update_max_health_by(2)
