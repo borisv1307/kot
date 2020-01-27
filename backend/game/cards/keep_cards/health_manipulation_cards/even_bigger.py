1d8668697a3e8b2f15223ee7dff8de98e0020676
Chris Vennel <chris@vennel.net>
2020-01-26 20:56:09 -0500

    updated even bigger

--- backend/game/cards/keep_cards/health_manipulation_cards/even_bigger.py
+++ backend/game/cards/keep_cards/health_manipulation_cards/even_bigger.py
@@ -5,15 +5,15 @@ from game.cards.keep_card import KeepCard
 class EvenBigger(KeepCard):
     def __init__(self):
         super(KeepCard, self).__init__("Even Bigger", 4,
-                                         "Your maximum [Heart] is increased by 2. Gain 2[Heart] when you get this card",
-                                         "")
+                                       "Your maximum [Heart] is increased by 2. Gain 2[Heart] when you get this card",
+                                       "")
 
     def immediate_effect(self, player_that_bought_the_card, other_players):
-        EvenBigger().special_effect(player_that_bought_the_card, None)
+        player_that_bought_the_card.update_max_health_by(2)
+        player_that_bought_the_card.update_health_by(2)
 
     def discard_effect(self, player_that_bought_the_card, other_players):
         player_that_bought_the_card.update_max_health_by(-2)
 
     def special_effect(self, player_that_bought_the_card, other_players):
-        player_that_bought_the_card.update_max_health_by(2)
-        player_that_bought_the_card.update_health_by(2)
+        pass
