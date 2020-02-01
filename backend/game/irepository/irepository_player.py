563e3eb56e7b63aca74cb4d545148e465416fda5
Oyesanmi <taiwo.oyesanmi@accenture.com>
2020-02-01 16:35:13 -0500

    repository pattern to support play

--- backend/game/irepository/irepository_player.py
+++ backend/game/irepository/irepository_player.py
@@ -1,6 +1,5 @@
 import datetime
 
-from passlib.hash import md5_crypt as md5
 from game.models import User
 from game.player.player import Player
 
@@ -12,7 +11,6 @@ class IRepositoryPlayer:
     def save_player_db(self, player: Player):
         self.user = User(monster_name=player.monster_name,
                          username=player.username,
-                         password=md5.encrypt(player.password),
                          date_created=datetime.datetime.now())
         self.user.save()
         return self.user
@@ -24,7 +22,6 @@ class IRepositoryPlayer:
     def update_player_db(self, player: Player):
         self.user = User.objects.get(monster_name=player.monster_name)
         self.user.username = player.username
-        self.user.password = md5.encrypt(player.password)
         self.user.save()
         return self.user
 
