eb74d989f6ddf6da051f8dae456b81c9d9bccf26
chris <chris@vennel.net>
2020-02-10 19:41:49 -0500

    renamed card's type to card_type to change name away from python builtin

--- backend/game/cards/discard_card.py
+++ backend/game/cards/discard_card.py
@@ -6,7 +6,7 @@ from game.cards.card import Card
 class DiscardCard(Card):
     def __init__(self, name, cost, effect, footnote=None):
         super().__init__(name, cost, effect, footnote)
-        self.type = "Discard"
+        self.card_type = "Discard"
 
     @abstractmethod
     def immediate_effect(self, player_that_bought_the_card, other_players):
