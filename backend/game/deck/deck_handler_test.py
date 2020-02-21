c4a6b04ffc278a2393f87d0fa2790ad273961d8a
Chris Vennel <chris@vennel.net>
2020-02-20 19:04:27 -0500

    update tests to accept discard cards auto discarding

--- backend/game/deck/deck_handler_test.py
+++ backend/game/deck/deck_handler_test.py
@@ -2,11 +2,11 @@ from typing import List
 
 import pytest
 
+import game.cards.master_card_list as master_card_list
 import game.values.constants as constants
 from game.cards.card import Card
+from game.cards.keep_card import KeepCard
 from game.deck.deck_handler import DeckHandler
-
-import game.cards.master_card_list as master_card_list
 from game.player.player import Player
 
 NUMBER_OF_CARDS_IN_GAME = len(master_card_list.get_all_cards())
@@ -38,18 +38,27 @@ def test_buy_single_card(deck_handler, rich_player):
     assert len(deck_handler.store) == constants.CARD_STORE_SIZE_LIMITER
     assert len(deck_handler.draw_pile) == NUMBER_OF_CARDS_IN_GAME - constants.CARD_STORE_SIZE_LIMITER - 1
     assert not deck_handler.draw_pile.__contains__(bought_card)
-    assert rich_player.has_instance_of_card(bought_card)
+    while True:
+        rich_player.update_energy_by(100)
+        card_to_purchase = deck_handler.store[0]
+        deck_handler.buy_card_from_store(0, rich_player)
+        if isinstance(card_to_purchase, KeepCard):
+            assert rich_player.has_instance_of_card(card_to_purchase)
+            break
 
 
 def test_discard_card(deck_handler, rich_player):
     assert len(deck_handler) == NUMBER_OF_CARDS_IN_GAME
-    card_player_will_buy = deck_handler.store[1]
-    deck_handler.buy_card_from_store(1, rich_player)
+    while True:
+        card_player_will_buy = deck_handler.store[0]
+        deck_handler.buy_card_from_store(0, rich_player)
+        if isinstance(card_player_will_buy, KeepCard):
+            # exit after buying one keep card
+            break
     assert len(deck_handler) == NUMBER_OF_CARDS_IN_GAME - 1
     assert rich_player.cards.__contains__(card_player_will_buy)
     deck_handler.discard(card_player_will_buy, rich_player.cards)
     assert len(deck_handler) == NUMBER_OF_CARDS_IN_GAME
-    assert len(deck_handler.discard_pile) == 1
     assert len(rich_player.cards) == 0
 
 
@@ -61,9 +70,12 @@ def test_cards_cost_energy(deck_handler, rich_player):
 
 
 def test_cards_bought_are_added_to_player(deck_handler, rich_player):
-    card_player_will_buy = deck_handler.store[1]
-    deck_handler.buy_card_from_store(1, rich_player)
-    assert rich_player.cards.__contains__(card_player_will_buy)
+    while True:
+        card_player_will_buy = deck_handler.store[0]
+        deck_handler.buy_card_from_store(0, rich_player)
+        if isinstance(card_player_will_buy, KeepCard):
+            assert rich_player.has_instance_of_card(card_player_will_buy)
+            break
 
 
 def test_force_shuffle_discard_add_to_draw(deck_handler, rich_player):
@@ -72,60 +84,28 @@ def test_force_shuffle_discard_add_to_draw(deck_handler, rich_player):
     # buy all cards but the 3 in the store, reset energy to never run out for test
     for _ in range(NUMBER_OF_CARDS_IN_GAME - 3):
         rich_player.energy = 100
-        bought_cards.append(deck_handler.buy_card_from_store(1, rich_player))
-    assert len(bought_cards) == NUMBER_OF_CARDS_IN_GAME - 3
+        deck_handler.buy_card_from_store(1, rich_player)
 
-    # discard the 5 cards_old_dir
-    for card in bought_cards:
-        deck_handler.discard(card)
-    bought_cards.clear()
-    assert len(deck_handler) == NUMBER_OF_CARDS_IN_GAME
-    assert len(deck_handler.discard_pile) == NUMBER_OF_CARDS_IN_GAME - constants.CARD_STORE_SIZE_LIMITER
+    assert len(deck_handler.draw_pile) == 0
 
-    # shuffle the discarded cards and add to the draw pile
-    deck_handler.shuffle_discard_pile_to_draw_pile()
-    assert len(deck_handler) == NUMBER_OF_CARDS_IN_GAME
-    assert len(deck_handler.discard_pile) == 0
+    deck_handler.buy_card_from_store(0, rich_player)
+
+    assert len(deck_handler.draw_pile) > 0
 
 
 def test_auto_reshuffle(deck_handler, rich_player):
-    # bought_cards = list()
-
     # buy all cards but 1 and the store
     for _ in range(NUMBER_OF_CARDS_IN_GAME - (1 + constants.CARD_STORE_SIZE_LIMITER)):
         rich_player.energy = 100
-        # bought_cards.append(deck_handler.buy_card_from_store(1, rich_player))
         deck_handler.buy_card_from_store(1, rich_player)
-    assert len(deck_handler) == 1 + constants.CARD_STORE_SIZE_LIMITER
-
-    # move discard all bought cards
-    while len(rich_player.cards) > 0:
-        deck_handler.discard(rich_player.cards.pop())
-
-    assert len(rich_player.cards) == 0
-    assert len(deck_handler) == NUMBER_OF_CARDS_IN_GAME
+    #  Now there's only one in the draw pile, and the three in the store
     assert len(deck_handler.draw_pile) == 1
 
     # buy all card but 1 and the store again, forcing reshuffle discard to draw pile, reset energy to never run out
     for _ in range(NUMBER_OF_CARDS_IN_GAME - (1 + constants.CARD_STORE_SIZE_LIMITER)):
         rich_player.energy = 100
         deck_handler.buy_card_from_store(0, rich_player)
-    assert len(rich_player.cards) == NUMBER_OF_CARDS_IN_GAME - (1 + constants.CARD_STORE_SIZE_LIMITER)
-    assert len(deck_handler.store) == constants.CARD_STORE_SIZE_LIMITER
-    assert len(deck_handler) == 1 + constants.CARD_STORE_SIZE_LIMITER
 
-
-def test_run_out_of_cards(deck_handler, rich_player):
-    # buy enough cards_old_dir to empty draw pile
-    for _ in range(NUMBER_OF_CARDS_IN_GAME - 3):
-        (deck_handler.buy_card_from_store(0, rich_player))
-    assert len(deck_handler) == 3
-    assert len(deck_handler.draw_pile) == 0
-
-    # next card should raise an exception in deck_handler.fill_card_store
-    with pytest.raises(Exception) as exception:
-        assert deck_handler.buy_card_from_store(0, rich_player)
-    assert str(exception.value) == constants.OUT_OF_CARDS_MSG
 
 def test_do_not_sell_card_to_player_with_no_energy(deck_handler):
     poor_player = Player()
@@ -133,7 +113,7 @@ def test_do_not_sell_card_to_player_with_no_energy(deck_handler):
     with pytest.raises(Exception) as raised_exception:
         deck_handler.buy_card_from_store(1, poor_player)
     assert constants.INSUFFICIENT_FUNDS_MSG in str(raised_exception.value)
-    
+
 
 def test_sweep_store_not_allowed_with_insufficient_energy(deck_handler):
     test_player = Player()
@@ -171,4 +151,3 @@ def test_sweep_store_costs_energy(deck_handler):
 def test_store_refills_after_sweep(deck_handler):
     test_sweep_store_costs_energy(deck_handler)
     assert len(deck_handler.store) == constants.CARD_STORE_SIZE_LIMITER
-
