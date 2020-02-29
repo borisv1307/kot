c9219edd16e344a5d9c6cbfa940d580434ff500a
chris <chris@vennel.net>
2020-02-29 15:39:13 -0500

    lock buy cards to current player

--- frontend/src/containers/Gameboard.js
+++ frontend/src/containers/Gameboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./Gameboard.css";
 import config from "../services/config";
 
@@ -8,85 +8,98 @@ import CardStore from "../components/Cards/CardStore";
 import PlayerValuesDisplay from "./../components/PlayerValues/PlayerValueDisplay";
 
 import GameInstance from "./../services/gameService";
+import YieldAlert from "../components/YieldAlert/YieldAlert";
 
 export default class GameboardLayout extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    let userName = "Guest_1234";
-    let roomName = "Room_1234";
-    if (props.location && props.location.state) {
-      userName = props.location.state.username;
-      roomName = props.location.state.gameRoom;
+        let userName = "Guest_1234";
+        let roomName = "Room_1234";
+        if (props.location && props.location.state) {
+            userName = props.location.state.username;
+            roomName = props.location.state.gameRoom;
+        }
+
+        this.state = {
+            username: userName,
+            gameRoom: roomName,
+            loggedIn: true
+        };
+
+        GameInstance.connect(this.state.gameRoom, config.GAME_SOCKET_API_PATH);
+        GameInstance.addYieldCallback(this.showAlert.bind(this));
+        this.yieldAlert = new YieldAlert(this.state.username)
+
     }
 
-    this.state = {
-      username: userName,
-      gameRoom: roomName,
-      loggedIn: true
-    };
+    showAlert(message) {
+        let username_whos_turn_it_is = message.user;
+        let yieldAlert = new YieldAlert(this.props, this.state.username, this.state.gameRoom)
+        if (this.state.username === username_whos_turn_it_is) {
+            yieldAlert.showCustom()
+        }
 
-    GameInstance.connect(this.state.gameRoom, config.GAME_SOCKET_API_PATH);
-  }
+    }
 
-  render() {
-    return (
-      <div className="board_container">
-        <br />
-        <h4>{this.state.gameRoom}</h4>
-        <div>
-          <div className="col">
-            <CardStore
-              currentUser={this.state.username}
-              currentRoom={this.state.gameRoom}
-            />
-          </div>
-        </div>
-        <div className="container">
-          {this.state.loggedIn ? (
-            <div>
-              <div className="row">
-                <div className="col-sm">
-                  <DiceRoller
-                    currentUser={this.state.username}
-                    currentRoom={this.state.gameRoom}
-                  />
+    render() {
+        return (
+            <div className="board_container">
+                <br/>
+                <h4>{this.state.gameRoom}</h4>
+                <div>
+                    <div className="col">
+                        <CardStore
+                            currentUser={this.state.username}
+                            currentRoom={this.state.gameRoom}
+                        />
+                    </div>
                 </div>
-                <div className="col-sm board_middle_column">
-                  <GameConsole
-                    sendMessage={payload => {
-                      GameInstance.sendMessage(payload);
-                    }}
-                    currentUser={this.state.username}
-                    currentRoom={this.state.gameRoom}
-                  />
+                <div className="container">
+                    {this.state.loggedIn ? (
+                        <div>
+                            <div className="row">
+                                <div className="col-sm">
+                                    <DiceRoller
+                                        currentUser={this.state.username}
+                                        currentRoom={this.state.gameRoom}
+                                    />
+                                </div>
+                                <div className="col-sm board_middle_column">
+                                    <GameConsole
+                                        sendMessage={payload => {
+                                            GameInstance.sendMessage(payload);
+                                        }}
+                                        currentUser={this.state.username}
+                                        currentRoom={this.state.gameRoom}
+                                    />
+                                </div>
+                                <div className="col-sm">
+                                    <PlayerValuesDisplay
+                                        currentUser={this.state.username}
+                                        currentRoom={this.state.gameRoom}
+                                        displayOnlySelf={true}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-sm">
+                                    <PlayerValuesDisplay
+                                        currentUser={this.state.username}
+                                        currentRoom={this.state.gameRoom}
+                                        displayOnlySelf={false}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <p>Please login</p>
+                    )}
                 </div>
-                <div className="col-sm">
-                  <PlayerValuesDisplay
-                    currentUser={this.state.username}
-                    currentRoom={this.state.gameRoom}
-                    displayOnlySelf={true}
-                  />
+                <div className="col">
+                    <a href="https://icons8.com/">Images Provided by icons8.com</a>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col-sm">
-                  <PlayerValuesDisplay
-                    currentUser={this.state.username}
-                    currentRoom={this.state.gameRoom}
-                    displayOnlySelf={false}
-                  />
-                </div>
-              </div>
             </div>
-          ) : (
-            <p>Please login</p>
-          )}
-        </div>
-        <div className="col">
-          <a href="https://icons8.com/">Images Provided by icons8.com</a>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
