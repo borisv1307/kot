bbdd5c7cebb175c958246bf8a25f2a0508243805
chris <chris@vennel.net>
2020-02-29 21:26:40 -0500

    fixed conflicting mocks for tests, implemented code review

--- backend/game/cards/keep_cards/turn_manipulation_cards/GiantBrain.py
+++ backend/game/cards/keep_cards/turn_manipulation_cards/GiantBrain.py
@@ -5,11 +5,5 @@ class GiantBrain(KeepCard):
     def __init__(self):
         super().__init__("Giant Brain", 5, "You have 1 extra die roll each turn")
 
-    def immediate_effect(self, player_that_bought_the_card, other_players):
-        pass
-
-    def discard_effect(self, player_that_bought_the_card, other_players):
-        pass
-
     def special_effect(self, player_that_bought_the_card, other_players):
         pass
