5a30876929a69272995567c902679a3e94dcb86d
chris <chris@vennel.net>
2020-03-03 18:45:27 -0500

    initialized board.py cards that were causing errors

--- backend/game/engine/board.py
+++ backend/game/engine/board.py
@@ -1,4 +1,6 @@
 import game.values.constants as constants
+from game.cards.keep_cards.energy_manipulation_cards.energy_hoarder import EnergyHoarder
+from game.cards.keep_cards.energy_manipulation_cards.solar_powered import SolarPowered
 from game.cards.keep_cards.victory_point_manipulation_cards.urbavore import Urbavore
 from game.deck.deck_handler import DeckHandler
 from game.dice.dice_handler import DiceHandler
@@ -54,12 +56,12 @@ class BoardGame:
 
     def post_roll_actions(self, active_player):
         # TODO, refactor to other method
-        # if active_player.has_instance_of_card(EnergyHoarder()):
-        #     EnergyHoarder.special_effect(
-        #         active_player, self.players.get_all_alive_players_minus_current_player())
-        # if active_player.has_instance_of_card(SolarPowered()):
-        #     SolarPowered.special_effect(
-        #         active_player, self.players.get_all_alive_players_minus_current_player())
+        if active_player.has_instance_of_card(EnergyHoarder()):
+            EnergyHoarder().special_effect(
+                active_player, self.players.get_all_alive_players_minus_current_player())
+        if active_player.has_instance_of_card(SolarPowered()):
+            SolarPowered().special_effect(
+                active_player, self.players.get_all_alive_players_minus_current_player())
         self.check_if_winner(active_player)
 
     def get_next_player_turn(self):
